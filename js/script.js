96ce39a7775265f886e4475d71b376bd3b1da4ec
Maplor <maplor@163.com>
2014-10-21 20:10:05 +0800

    修复css和js问题，首页添加滚动图标题和卡片图片裁剪。

--- js/script.js
+++ js/script.js
@@ -1,89 +1,102 @@
 // JavaScript Document
 (function() {
 	jQuery.focus = function(slid) {
-		var sWidth = $(slid).width(); //获取焦点图的宽度（显示面积）
-		var $li = $(slid).find("ul li");
-		var len = $li.length; //获取焦点图个数
-		var index = 0;
-		var picTimer;
-		
-		//滚动图片作居中处理
-		var showW = $li.width();
-		var showH = $li.height();
-		$li.find("img").each(function (i, e) {
-			$(this).css({
-				"margin-left": -Math.abs(showW - $(this).width())/2,
-				"margin-top": -Math.abs(showH - $(this).height())/2
+		var $slid = $(slid)
+		if ($slid.length > 0) {
+			var sWidth = $slid.width(); //获取焦点图的宽度（显示面积）
+			var $li = $slid.find("ul li");
+			var len = $li.length; //获取焦点图个数
+			var index = 0;
+			var picTimer;
+			
+			//滚动图片作居中处理
+			var showW = $li.width();
+			var showH = $li.height();
+			$li.find("img").each(function (i, e) {
+				$(this).css({
+					"margin-left": -Math.abs(showW - $(this).width())/2,
+					"margin-top": -Math.abs(showH - $(this).height())/2
+				})
 			})
-		})
+			
+			//以下代码添加数字按钮和按钮后的半透明条，还有上一页、下一页两个按钮
+			//var btn = "<div class='btnBg'></div><div class='btn'>";  //带半透明条
+			var btn = ""; //"<div class='btn'" +"style='margin-left:-"+ (28*len+20)/2 +"px'>";  //不带半透明条
+//			for(var i=0; i < len; i++) {
+//				var ii = i+1;
+//				//btn += "<span>"+ii+"</span>"; //带数字
+//				btn += "<span>" + "</span>"; //不带数字
+//			}
+			btn += "</div><div class='preNext pre'><div class='pre-btn'></div></div><div class='preNext next'><div class='next-btn'></div></div>";
+			$slid.append(btn);
+			//$slid.find("div.btnBg").css("opacity",0.5);
 		
-		//以下代码添加数字按钮和按钮后的半透明条，还有上一页、下一页两个按钮
-		//var btn = "<div class='btnBg'></div><div class='btn'>";  //带半透明条
-		var btn = "<div class='btn'" +"style='margin-left:-"+ (28*len+20)/2 +"px'>";  //不带半透明条
-		for(var i=0; i < len; i++) {
-			var ii = i+1;
-			//btn += "<span>"+ii+"</span>"; //带数字
-			btn += "<span>" + "</span>"; //不带数字
-		}
-		btn += "</div><div class='preNext pre'><div class='pre-btn'></div></div><div class='preNext next'><div class='next-btn'></div></div>";
-		$(slid).append(btn);
-		//$(slid).find("div.btnBg").css("opacity",0.5);
-	
-		//为小按钮添加鼠标滑入事件，以显示相应的内容
-		$(slid+" div.btn span").css("opacity",0.8).mouseenter(function() {
-			index = $(slid+" .btn span").index(this);
-			showPics(index);
-		}).eq(0).trigger("mouseenter");
-	
-		//上一页、下一页按钮透明度处理
-		$(slid+" .preNext").css("opacity",0.6).hover(function() {
-			$(this).stop(true,false).animate({"opacity":"0.8"},200);
-		},function() {
-			$(this).stop(true,false).animate({"opacity":"0.6"},200);
-		});
-	
-		//上一页按钮
-		$(slid+" .pre").click(function() {
-			index -= 1;
-			if(index == -1) {index = len - 1;}
-			showPics(index);
-		});
-	
-		//下一页按钮
-		$(slid+" .next").click(function() {
-			index += 1;
-			if(index == len) {index = 0;}
-			showPics(index);
-		});
-	
-		//本例为左右滚动，即所有li元素都是在同一排向左浮动，所以这里需要计算出外围ul元素的宽度
-		//$(slid+" ul").css("width",sWidth * (len));
-
-		//当页面宽度改变时，相应改变数据，实现响应式
-//		$(window).resize(function(e) {
-//			sWidth = $(slid).width();
-//			len = $(slid).find("ul li").length;
-//		});
-
-		//鼠标滑上焦点图时停止自动播放，滑出时开始自动播放
-		$(slid).hover(function() {
-			clearInterval(picTimer);
-		},function() {
-			picTimer = setInterval(function() {
+			//为小按钮添加鼠标滑入事件，以显示相应的内容
+			$(slid+" div.btn span").css("opacity",0.8).mouseenter(function() {
+				index = $(slid+" .btn span").index(this);
 				showPics(index);
-				index++;
-				if(index == len) {index = 0;}
-			},5000); //此4000代表自动播放的间隔，单位：毫秒
-		}).trigger("mouseleave");
+			}).eq(0).trigger("mouseenter");
 		
-		//显示图片函数，根据接收的index值显示相应的内容
-		function showPics(index) { //普通切换
-			var nowLeft = -index*sWidth; //根据index值计算ul元素的left值
-			$(slid+" ul").stop(true,false).animate({"left":nowLeft},500); //通过animate()调整ul元素滚动到计算出的position
-			$(slid+" .btn span").removeClass("on").eq(index).addClass("on"); //为当前的按钮切换到选中的效果
-			$(slid+" .btn span").stop(true,false).animate({"opacity":"0.8"},300).eq(index).stop(true,false).animate({"opacity":"1"},300); //为当前的按钮切换到选中的效果
+			//上一页、下一页按钮透明度处理
+			$(slid+" .preNext").css("opacity",0.6).hover(function() {
+				$(this).stop(true,false).animate({"opacity":"0.8"},200);
+			},function() {
+				$(this).stop(true,false).animate({"opacity":"0.6"},200);
+			});
+		
+			//上一页按钮
+			$(slid+" .pre").click(function() {
+				index -= 1;
+				if(index == -1) {index = len - 1;}
+				showPics(index);
+			});
+		
+			//下一页按钮
+			$(slid+" .next").click(function() {
+				index += 1;
+				if(index == len) {index = 0;}
+				showPics(index);
+			});
+		
+			//本例为左右滚动，即所有li元素都是在同一排向左浮动，所以这里需要计算出外围ul元素的宽度
+			//$(slid+" ul").css("width",sWidth * (len));
+	
+			//当页面宽度改变时，相应改变数据，实现响应式
+	//		$(window).resize(function(e) {
+	//			sWidth = $(slid).width();
+	//			len = $(slid).find("ul li").length;
+	//		});
+	
+			//鼠标滑上焦点图时停止自动播放，滑出时开始自动播放
+			$slid.hover(function() {
+				clearInterval(picTimer);
+			},function() {
+				picTimer = setInterval(function() {
+					showPics(index);
+					index++;
+					if(index == len) {index = 0;}
+				},5000); //此4000代表自动播放的间隔，单位：毫秒
+			}).trigger("mouseleave");
+			
+			//显示图片函数，根据接收的index值显示相应的内容
+			function showPics(index) { //普通切换
+				var nowLeft = -index*sWidth; //根据index值计算ul元素的left值
+				$(slid+" ul").stop(true,false).animate({"left":nowLeft},500); //通过animate()调整ul元素滚动到计算出的position
+				$(slid+" .btn span").removeClass("on").eq(index).addClass("on"); //为当前的按钮切换到选中的效果
+				$(slid+" .btn span").stop(true,false).animate({"opacity":"0.8"},300).eq(index).stop(true,false).animate({"opacity":"1"},300); //为当前的按钮切换到选中的效果
+			}
 		}
 	};
+	
+	jQuery.cardImg = function (card) {
+		var $card = $(card);
+		if ($card.length > 0) {
+			$card.find("div.card-img").children("img").each(function (i, e) {
+				var showH = $(this).parent().height();
+				$(this).css("margin-top", -Math.abs(showH - $(this).height())/2);
+			})
+		}
+	}
 	
 //	$.fn.hoverDelay = function(options){
 //      var defaults = {
@@ -121,7 +134,10 @@
 $(document).ready(function () {
 	// 绑定二级菜单功能
 	$("#navigation").find("li.second").hover(function () {
-		$(this).children("ul").stop(true,false).slideToggle("fast");
+		$(this).children("ul").stop(true,false).slideDown("fast");
+	},
+	function () {
+		$(this).children("ul").stop(true,false).slideUp("fast");
 	});
 //	$("#navigation").delegate("li.second", "click", function () {
 //		$(this).children("ul").slideToggle();
@@ -130,7 +146,7 @@ $(document).ready(function () {
 	//绑定侧边栏功能
 	var $aside = $("#aside");
 	if ($aside.length > 0) {
-		//侧边栏固定位置
+		//侧边栏固定位置]
 		var top = $aside.offset().top;
 		$(window).scroll(function() {
 			if ($(window).scrollTop() > top) {
@@ -183,6 +199,8 @@ function indexLoad () {
 			t++;
 		}, 1000);
 		window.onload = function () {
+			$.focus("#focus");
+			
 			if (t > 2) {
 				$("#load").fadeOut("400");
 				clearInterval(timer);
